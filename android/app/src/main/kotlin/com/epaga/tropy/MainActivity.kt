88b7a1b5e1c17a18b5812c98296de8747b269489
John Goering <johngoering@me.com>
2025-03-10 19:32:29 +0100

    building on android and ios with flutter upgrade

---
+++ android/app/src/main/kotlin/com/epaga/tropy/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.epaga.tropy
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,9 +5,9 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
+import path_provider_foundation
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
